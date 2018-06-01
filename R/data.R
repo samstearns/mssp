210f730d3b68955d4b5cfa5c481a2ec7e69e925c
Sam Stearns <sam.stearns@gmail.com>
2018-06-01 16:48:07 -0400

    Updated version with data load

--- R/data.R
+++ R/data.R
@@ -1,53 +1,66 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+#' Downloads PUF files from CMS website (https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SSPACO/index.html)
+#'
+#' @param year MSSP performance year.
+#' @return Data frame with mssp data.
+#' @examples
+#' load_puf_file(2016)
+#' @export
+load_puf_file <- function(year) {
 
-hello <- function() {
-  print("Hello, world!")
+  if (!requireNamespace("RCurl", quietly = TRUE)) {
+    stop("Package \"pkg\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+
+  if (year == 2013) {
+    # Note 2013 has different data format
+    address <- "https://data.cms.gov/api/views/475s-fzi7/rows.csv?accessType=DOWNLOAD&bom=true"
+  } else if (year == 2014) {
+    address <- "https://data.cms.gov/api/views/888h-akbg/rows.csv?accessType=DOWNLOAD&bom=true"
+  } else if (year == 2015) {
+    address <- "https://data.cms.gov/api/views/7rrf-3gxr/rows.csv?accessType=DOWNLOAD&bom=true"
+  } else if (year == 2016) {
+    address <- "https://data.cms.gov/api/views/i3xi-3vkx/rows.csv?accessType=DOWNLOAD&bom=true"
+  } else {
+    address <- "https://data.cms.gov/api/views/475s-fzi7/rows.csv?accessType=DOWNLOAD&bom=true"
+  }
+
+  df <- LoadPUF(address);
+
+  if (year == 2013) {
+    # Note: 2013 data has a different format than 2014 and 2015
+  }
+
+  df$Performance_Year <- year
+
+  df$CMS_HCC_RiskScore_PY <- (df$CMS_HCC_RiskScore_DIS_PY * df$N_AB_Year_DIS_PY +
+       df$CMS_HCC_RiskScore_ESRD_PY * df$N_AB_Year_ESRD_PY +
+       df$CMS_HCC_RiskScore_AGDU_PY * df$N_AB_Year_AGED_Dual_PY +
+       df$CMS_HCC_RiskScore_AGND_PY * df$N_AB_Year_AGED_NonDual_PY) / df$N_AB
+
+
+  return (df)
 }
 
-
-# Load SSP PUF files from https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SSPACO/index.html
-
-# Load Libraries -----------------------------------------------------------------------------------------------
-library(RCurl)
-library(ggplot2)
-
-# Helper function to downlaod PUF files from CMS
+# Load the PUF files from the web.
 LoadPUF <- function(address) {
-
-  x <- getURL(address)
+  x <- RCurl::getURL(address)
   df <- read.csv(text = x)
   return (df)
 }
 
-# Load the PUF files from the web. Note: 2013 data has a different format than 2014 and 2015
-aco.2013 <- LoadPUF("https://data.cms.gov/api/views/475s-fzi7/rows.csv?accessType=DOWNLOAD&bom=true")
-aco.2014 <- LoadPUF("https://data.cms.gov/api/views/888h-akbg/rows.csv?accessType=DOWNLOAD&bom=true")
-aco.2015 <- LoadPUF("https://data.cms.gov/api/views/7rrf-3gxr/rows.csv?accessType=DOWNLOAD&bom=true")
-aco.2016 <- LoadPUF("https://data.cms.gov/api/views/i3xi-3vkx/rows.csv?accessType=DOWNLOAD&bom=true")
+
 
 
 # Add additional variables --------------------------------------------------------------------------------------
 
 # Population-weighted HCC Score
-aco.2015$CMS_HCC_RiskScore_PY <- (aco.2015$CMS_HCC_RiskScore_DIS_PY * aco.2015$N_AB_Year_DIS_PY +
-                                    aco.2015$CMS_HCC_RiskScore_ESRD_PY * aco.2015$N_AB_Year_ESRD_PY +
-                                    aco.2015$CMS_HCC_RiskScore_AGDU_PY * aco.2015$N_AB_Year_AGED_Dual_PY +
-                                    aco.2015$CMS_HCC_RiskScore_AGND_PY * aco.2015$N_AB_Year_AGED_NonDual_PY) / aco.2015$N_AB;
+#aco.2015$CMS_HCC_RiskScore_PY <- (aco.2015$CMS_HCC_RiskScore_DIS_PY * aco.2015$N_AB_Year_DIS_PY +
+#                                    aco.2015$CMS_HCC_RiskScore_ESRD_PY * aco.2015$N_AB_Year_ESRD_PY +
+#                                    aco.2015$CMS_HCC_RiskScore_AGDU_PY * aco.2015$N_AB_Year_AGED_Dual_PY +
+#                                    aco.2015$CMS_HCC_RiskScore_AGND_PY * aco.2015$N_AB_Year_AGED_NonDual_PY) / aco.2015$N_AB;
 
-aco.2014$CMS_HCC_RiskScore_PY <- (aco.2014$CMS_HCC_RiskScore_DIS_PY * aco.2014$N_AB_Year_DIS +
-                                    aco.2014$CMS_HCC_RiskScore_ESRD_PY * aco.2014$N_AB_Year_ESRD +
-                                    aco.2014$CMS_HCC_RiskScore_AGDU_PY * aco.2014$N_AB_Year_AGED_Dual +
-                                    aco.2014$CMS_HCC_RiskScore_AGND_PY * aco.2014$N_AB_Year_AGED_NonDual) / aco.2014$N_AB;
+#aco.2014$CMS_HCC_RiskScore_PY <- (aco.2014$CMS_HCC_RiskScore_DIS_PY * aco.2014$N_AB_Year_DIS +
+#                                    aco.2014$CMS_HCC_RiskScore_ESRD_PY * aco.2014$N_AB_Year_ESRD +
+ #                                   aco.2014$CMS_HCC_RiskScore_AGDU_PY * aco.2014$N_AB_Year_AGED_Dual +
+ #                                   aco.2014$CMS_HCC_RiskScore_AGND_PY * aco.2014$N_AB_Year_AGED_NonDual) / aco.2014$N_AB;
