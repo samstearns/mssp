f3c0cf7d863d89d54c13d3ea79589d18005b0f05
Sam Stearns <sam.stearns@gmail.com>
2018-06-07 01:08:10 -0400

    Added expenditure profile

--- R/profile.R
+++ R/profile.R
@@ -24,31 +24,52 @@ lookup_aco_by_state <- function(df, str) {
   df[matches, 1:2]
 }
 
-#' Displays utilization metrics
+#' Profiles utilization metrics vs. national sample
 #' @param df SSP data
 #' @param aco_num ACO Number.
 #' @param year MSSP performance year.
 #' @return Data frame with mssp data.
 #' @examples
-#' profile_utilization(df, 2016)
+#' profile_utilization(df, "A95164", 2016)
 #' @export
 profile_utilization <- function(df, aco_num, year = NULL) {
   utilization_variables <- c("ACO_Num", "ACO_Name", "ADM", "ADM_S_Trm", "ADM_L_Trm", "ADM_Rehab",  "ADM_Psych", "readm_Rate_1000", "prov_Rate_1000",
                              "P_SNF_ADM", "P_EDV_Vis", "P_EDV_Vis_HOSP", "P_CT_VIS", "P_MRI_VIS", "P_EM_Total", "P_EM_PCP_Vis", "P_EM_SP_Vis")
 
+  profile_aco(df, aco_num, utilization_variables)
+}
+
+#' Profiles expenditures vs. national sample
+#' @param df SSP data
+#' @param aco_num ACO Number.
+#' @param year MSSP performance year.
+#' @return Data frame with mssp data.
+#' @examples
+#' profile_expenditure(df, "A95164", 2016)
+#' @export
+profile_expenditures <- function(df, aco_num, year = NULL) {
+  expenditures_variables <- c("ACO_Num", "ACO_Name", "CapAnn_INP_All", "CapAnn_INP_S_trm", "CapAnn_INP_L_trm", "CapAnn_INP_Rehab",  "CapAnn_INP_Psych", "readm_Rate_1000", "prov_Rate_1000",
+                             "CapAnn_HSP", "CapAnn_SNF", "CapAnn_INP_Other", "CapAnn_OPD", "CapAnn_PB", "CapAnn_AmbPay", "CapAnn_HHA", "CapAnn_DME")
+
+  profile_aco(df, aco_num, expenditures_variables)
+}
+
+
+profile_aco <- function(df, aco_num, profile_variables, year = NULL) {
+
   # get utilization for the selected ACO
   # TODO: filter for year
-  aco_results <- df[which(df$ACO_Num==aco_num), utilization_variables]
+  aco_results <- df[which(df$ACO_Num==aco_num), profile_variables]
 
   # Calculate median values
-  median_results <- data.frame(matrix(ncol = 17, nrow = 2))
-  colnames(median_results) <- utilization_variables
+  median_results <- data.frame(matrix(ncol = length(profile_variables), nrow = 2))
+  colnames(median_results) <- profile_variables
   median_results[1,1] <- "N/A"
   median_results[1,2] <- "Median"
   median_results[1,1] <- "N/A"
   median_results[1,2] <- "% Diff vs. Median"
   i <- 3
-  for (var in uv[3:17]) {
+  for (var in uv[3:length(profile_variables)]) {
     median_results[1, i] <- median(df[,var])
     median_results[2, i] <- aco_results[1, i] * 1.0 / median_results[1, i] - 1.0
     i <- i + 1
@@ -57,17 +78,3 @@ profile_utilization <- function(df, aco_num, year = NULL) {
   # join the ACO and median values
   aco_results <- rbind(aco_results, median_results)
 }
-
-#CapAnn_INP_All
-#CapAnn_INP_S_trm
-#CapAnn_INP_L_trm
-#CapAnn_INP_Rehab
-#CapAnn_INP_Psych
-#CapAnn_HSP
-#CapAnn_SNF
-#CapAnn_INP_Other
-#CapAnn_OPD
-#CapAnn_PB
-#CapAnn_AmbPay
-#CapAnn_HHA
-#CapAnn_DME
