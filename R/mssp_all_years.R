ce3e87ffccd7872a8a66866ec80439fcb288452c
Sam Stearns <sam.stearns@gmail.com>
2019-07-08 06:21:30 -0400

    Consolidated dataset - initial checkin

---
+++ R/mssp_all_years.R
@@ -0,0 +1,13 @@
+#' MSSP data from all years, saved in the 2017 column format.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 1849 rows and 16 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"mssp_all_years"
