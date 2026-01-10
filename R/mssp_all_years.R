72a1ff1522ae8509a0f9e2e1eede90a59dcb31e8
Sam Stearns <sam.stearns@gmail.com>
2026-01-10 16:12:06 -0500

    Added docs

--- R/mssp_all_years.R
+++ R/mssp_all_years.R
@@ -5,3 +5,12 @@
 #' @format A data frame with 1849 rows and 166 variables:
 #' @source \url{https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SSPACO/index.html}
 "mssp_all_years"
+
+
+#' Crosswalk of ACO IDs to ACO Numbers data from all years.
+#'
+#' A dataset providing a crosswalk between ACO ID and ACO NUM.
+#'
+#' @format A data frame with 767 rows and 3 variables:
+#' @source \url{https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SSPACO/index.html}
+"aco_id_crosswalk"
