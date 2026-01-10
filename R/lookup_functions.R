2724c95bbafce40e5158bbcd25a41b07dfba50d8
Sam Stearns <sam.stearns@gmail.com>
2026-01-10 15:52:15 -0500

    Updated documentation

--- R/lookup_functions.R
+++ R/lookup_functions.R
@@ -3,7 +3,8 @@
 #' @param str Search string with the name of the ACO
 #' @return Potential matches
 #' @examples
-#' lookup_aco_num("Mercy")
+#' a <- load_puf_file(2022)
+#' lookup_aco_num(a, "Mercy")
 #' @export
 lookup_aco_num <- function(df, str) {
   matches <- grep(str, df$aco_name)
@@ -16,7 +17,8 @@ lookup_aco_num <- function(df, str) {
 #' @param str Name of the state
 #' @return ACOs where assigned beneficiaries reside in the state
 #' @examples
-#' lookup_aco_by_state("Massachusetts")
+#' a <- load_puf_file(2022)
+#' lookup_aco_by_state(a, "Maine")
 #' @export
 lookup_aco_by_state <- function(df, str) {
   matches <- grep(str, df$aco_state)
