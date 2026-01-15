816f0b996961b7185f7a0fb16c6f70c73560989b
Sam Stearns <sam.stearns@gmail.com>
2026-01-15 07:08:15 -0500

    Broke out separate documentation file

---
+++ R/aco_id_crosswalk.R
@@ -0,0 +1,7 @@
+#' Crosswalk of ACO IDs to ACO Numbers data from all years.
+#'
+#' A dataset providing a crosswalk between ACO ID and ACO NUM. This crosswalk from CMS provides a link between the 5-character ACO ID used in Shared Savings Program public use and research identifiable data files produced in 2019 and later and the 6-character Encrypted ACO ID used in files produced
+#'
+#' @format A data frame with 767 rows and 3 variables:
+#' @source \url{https://www.cms.gov/Research-Statistics-Data-and-Systems/Downloadable-Public-Use-Files/SSPACO/index.html}
+"aco_id_crosswalk"
